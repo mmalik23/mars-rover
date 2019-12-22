99bdd54db832dd8caa2387330ac69e7cd4c38cdc
mmalik23 <muqeetamalik@gmail.com>
2019-12-22 12:11:01 +0000

    be able to play the game

--- src/main/scala/mars/Mars.scala
+++ src/main/scala/mars/Mars.scala
@@ -4,15 +4,47 @@ import scala.io.StdIn
 import scala.util.Random
 
 class Mars(
-            writer: String => Unit =  println,
-            rows : => Int =  Random.between(10,100),
-            columns : => Int = Random.between(10,100)
+            writer: String => Unit = println,
+            input:  () => String = StdIn.readLine,
+            rows : Int =  Random.between(10,100),
+            columns : Int = Random.between(10,100)
           ) {
+
   def run(): Unit = {
     val grid = Grid(rows, columns)
     val initialRover = Rover(Coordinates(rows/ 2, columns /2), Direction.North)
     writer(grid.toString())
     writer(initialRover.toString())
+    writer("w: Move Forward")
+    writer("a: Rotate anti-clockwise")
+    writer("d: Rotate clockwise")
+    writer("e: to end game")
+    writer("remember to press enter afterwards!")
+
+    letsPlay(grid, input)(initialRover)
+
+  }
+
+  final def letsPlay(grid: Grid, input: () => String)(rover: Rover): Unit = {
+
+    val recurse = letsPlay(grid, input) _
+    def translate(rover: Rover): Rover = rover.copy(coordinates = rover.coordinates.translate(grid))
+    def consoleLog(rover: Rover): Rover = {
+      writer(rover.toString)
+      rover
+    }
+    val f = (rover.relocate _).andThen(translate).andThen(consoleLog).andThen(recurse)
+
+    input() match {
+      case "w" => f(MoveForward)
+      case "a" => f(RotateAntiClockwise)
+      case "d" => f(RotateClockwise)
+      case "e" => writer("cya")
+      case _ =>
+        writer("something weird happened: try agauin")
+        recurse(rover)
+
+    }
   }
 }
 
@@ -33,7 +65,7 @@ case class Rover(coordinates: Coordinates, direction: Direction.Direction) {
     case RotateClockwise     => Rover(coordinates, Direction.clockwise(direction))
   }
 
-  override def toString: String = s"The rover is currently sitting at $coordinates, facing $direction"
+  override def toString: String = s"The rover is at coordinates $coordinates, facing $direction"
 }
 
 case class Coordinates(x: Int, y: Int) {
--- src/test/scala/mars/MarsTest.scala
+++ src/test/scala/mars/MarsTest.scala
@@ -6,16 +6,25 @@ import scala.collection.mutable.ArrayBuffer
 
 class MarsTest extends AnyFunSuite {
 
-  test("Can make a grid and show the world!") {
+  test("Can make a grid, show the world! and end the game!") {
     val messages = new ArrayBuffer[String]()
 
-    new Mars(messages.append, 1, 2).run()
+    new Mars(messages.append, () => "e", 1, 2).run()
 
     assert(messages.toList ===
       List(
         "Welcome to Mars! Mars has a waistline of 1 blocks and a height of 2 blocks",
-        "Welcome to Mars! Mars has a waistline of 1 blocks and a height of 2 blocks", "The rover is currently sitting at (0,1), facing North"
-      ))
+        "The rover is at coordinates (0,1), facing North",
+        "w: Move Forward",
+        "a: Rotate anti-clockwise",
+        "d: Rotate clockwise",
+        "e: to end game",
+        "remember to press enter afterwards!",
+        "cya"
+      )
+    )
+
+
 
   }
 
